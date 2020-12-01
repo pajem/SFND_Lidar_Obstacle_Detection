2dc449c55376f001277aa7e0a679581d8320d321
Paulo Morales <pajemorales@gmail.com>
2020-12-01 22:11:03 +0900

    implement search in kdtree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -19,6 +19,8 @@ struct Node
 
 struct KdTree
 {
+	static constexpr int AXIS_COUNT = 2;
+
 	Node* root;
 
 	KdTree()
@@ -34,6 +36,7 @@ struct KdTree
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		searchNode(ids, target, distanceTol, root, 0);
 		return ids;
 	}
 
@@ -51,15 +54,56 @@ private:
 			// create root node
 			parent = new Node(value, id);
 		} else {
-			int axisIndex = depth % 2; // 2D point
+			int axisIndex = depth % AXIS_COUNT;
 			if (value[axisIndex] < parent->point[axisIndex]) {
 				// left
-				insertNode(parent->left, value, id, ++depth);
+				insertNode(parent->left, value, id, depth + 1);
 			} else {
 				// right
-				insertNode(parent->right, value, id, ++depth);
+				insertNode(parent->right, value, id, depth + 1);
 			}
 		}
+	}
+
+	void searchNode(std::vector<int>& ids, const std::vector<float>& targetPoint, float tolerance, Node*& currentNode, int depth) {
+		if (currentNode == nullptr) {
+			// empty tree
+			return;
+		} else {
+			auto& currentPoint = currentNode->point;
+			if (isInsideToleranceBox(targetPoint, currentPoint, tolerance)) {
+				if (distance(targetPoint, currentPoint) <= tolerance) {
+					ids.push_back(currentNode->id);
+				}
+			}
+			int axisIndex = depth % AXIS_COUNT;
+			// traverse left
+			if (currentPoint[axisIndex] > (targetPoint[axisIndex] - tolerance)) {
+				searchNode(ids, targetPoint, tolerance, currentNode->left, depth + 1);
+			}
+			// traverse right
+			if (currentPoint[axisIndex] < (targetPoint[axisIndex] + tolerance)) {
+				searchNode(ids, targetPoint, tolerance, currentNode->right, depth + 1);
+			}
+		}
+	}
+
+	bool isInsideToleranceBox(const std::vector<float>& targetPoint, const std::vector<float>& point, float tolerance) {
+		bool inside = true;
+		for (int i = 0; i < AXIS_COUNT; ++i) {
+			auto minValue = targetPoint[i] - tolerance;
+			auto maxValue = targetPoint[i] + tolerance;
+			inside &= (point[i] >= minValue) && (point[i] <= maxValue);
+		}
+		return inside;
+	}
+
+	float distance(const std::vector<float>& p1, const std::vector<float>& p2) {
+		float distance = 0;
+		for (int i = 0; i < AXIS_COUNT; ++i) {
+			distance += (p1[i] - p2[i]) * (p1[i] - p2[i]);
+		}
+		return std::sqrt(distance);
 	}
 };
 
